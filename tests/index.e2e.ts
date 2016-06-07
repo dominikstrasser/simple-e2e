127763d620ee4974bccc6e46fd378f11092120ff
Dominik Strasser <dominik.strasser@netural.com>
2016-06-07 23:01:08 +0200

    added some demo tests

--- tests/index.e2e.ts
+++ tests/index.e2e.ts
@@ -10,7 +10,23 @@ describe ('The Github Page', () => {
   })
   
   it('should have a icon', () => {
-    page.navigateTo();
+    page.navigateToOfflineApplication();
     expect(page.getHeaderLogo().isDisplayed()).toBeTruthy();
   });
+
+  it('should wait 3 seconds and change to the author site', () => {
+    page.navigateToOfflineApplication();
+    browser.sleep(3000);
+    page.getAuthorLink().click();
+    expect(browser.getCurrentUrl()).toBe('https://github.com/dominikstrasser');
+  });
+
+  it('should find simple-e2e in the search results', () => {
+    page.navigateToDominikStrasser();
+    page.getRepositoryLink().click();
+    browser.sleep(3000);
+    page.getSearchInput().sendKeys('simple-e2e');
+    browser.sleep(3000);
+    expect(page.getSeachResultItems().get(0).getText()).toBe('simple-e2e');
+  })
 }) 
--- tests/index.po.ts
+++ tests/index.po.ts
@@ -1,11 +1,31 @@
 export class IndexPage {
   
-  navigateTo() {
+  navigateToOfflineApplication() {
     return browser.get('/dominikstrasser/angular2-offline-application');
+  }
+
+  navigateToDominikStrasser() {
+    return browser.get('/dominikstrasser')
   }
   
   getHeaderLogo () {
     return element(by.css('.header-logo-invertocat'))
   }
+
+  getAuthorLink () {
+    return element(by.css('.author'));
+  }
   
+  getRepositoryLink () {
+    return element(by.linkText('Repositories'));
+  }
+
+  getSearchInput () {
+    return element(by.id('your-repos-filter'));
+  }
+
+  getSeachResultItems () {
+    return element.all(by.css('.repo-list-name'));
+  }
+
 }
